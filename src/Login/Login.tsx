140cf979001f9b07dc55f7295f875defdfd48ade
vebsun95 <veb.sundal@gmail.com>
2022-09-16 15:38:41 +0200

    feat: login now implementes react-hook-form

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,53 +1,61 @@
 import "./Login.css"
-import { useState, useEffect, useContext } from "react";
+import { useForm } from 'react-hook-form'
+import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../Context/UserContext";
+import { User, UserContext } from "../Context/UserContext";
+import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+import { storageSave, storageRead } from "../Storage/Storage";
 
-const API_USER_URI = "https://ove-noroff-api.herokuapp.com/users";
-interface user {
-    id: number,
+const API_USER_URI = "https://ove-noroff-api.herokuapp.com/translations";
+
+interface FormValues {
     username: string,
-    favourites: string[],
 }
 
+
 function Login() {
-    const [username, setUsername] = useState("");
-    const [error, setError] = useState("");
-    const [user, setUser] = useContext(UserContext);
+
+    const [error, setError] = useState<Error | null>(null);
+
+    const { register, handleSubmit, formState: { errors: formErrors } } = useForm<FormValues>();
+
+    const [, setUser] = useContext(UserContext);
+
     const nav = useNavigate();
 
-    const onChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setUsername(event.currentTarget.value);
-    }
+    // Silent login on-load;
+    useEffect(() => {
+        let user = storageRead('translate-user');
+        if(user === null)
+            return;
+        setUser(user);
+        nav("/translation");
+    });
 
-    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        fetch(API_USER_URI)
+    // Fetches users with username === to form value, if any logs in as first found.
+    const onSubmit = (values: FormValues) => {
+        fetch(API_USER_URI + '?username=' + values.username)
             .then(response => response.json())
-            .then((users: user[]) => {
-                console.log(users)
-                if (!users.some(user => user.username === username))
-                    throw new Error(`There are no users named ${username}`);
-                setUser({name: users[0].username});
-                nav("/translation");
+            .then((users: User[]) => {
+                if (users.length === 0)
+                    throw new Error("No users with that name");
+                setUser(users[0]); // Update UserContext to store the logged in User;
+                storageSave("translate-user", users[0]); // Save the loggend in user in local storage.
+                nav("/translation"); // Navigate to the translation page.
             })
-            .catch((error: Error) => {
-                setError(error.message);
-                // Clear error message after timer
-                setTimeout(() => {
-                    setError("");
-                }, 5000);
-            });
+            .catch(error => { setError(error) });
     }
 
     return (
         <div className="Container">
-            <form onSubmit={submitHandler} className="Form">
-                <input placeholder="What's your name?" onChange={onChangeHandler}></input>
-                <button type="submit">&rarr;</button>
+            <form className="Form">
+                <input placeholder="What's your name?" {...register("username", { required: ("Username is required to login") })} />
+                <BsFillArrowRightCircleFill  className="Submit-Btn" type="submit" onClick={handleSubmit(onSubmit)}/>
             </form>
-            <p className="Error">{error}</p>
-        </div>)
+            <p className="Error">{formErrors.username && formErrors.username.message
+                || error && error.message}</p>
+        </div>
+    );
 }
 
 export default Login;

641e6946297c70ad32eaa82cef47c090352f7ebd
vebsun95 <veb.sundal@gmail.com>
2022-09-16 09:26:32 +0200

    feat: UserContext implementation

--- src/Context/UserContext.tsx
+++ src/Context/UserContext.tsx
@@ -4,12 +4,13 @@ interface User {
     name: string,
 }
 
-type setUser = React.Dispatch<React.SetStateAction<null>>
+type setUser = React.Dispatch<React.SetStateAction<User | null>>
+
 
 export const UserContext = createContext<[User | null, setUser]>(null as unknown as [null, setUser]);
 
-function UserProvider({ children } : { children : JSX.Element[]}) {
-    const [user, setUser] = useState(null);
+function UserProvider({ children } : { children : JSX.Element | JSX.Element[]}) {
+    const [user, setUser] = useState<User | null>(null);
 
     return (
         <UserContext.Provider value={[ user, setUser ]}>

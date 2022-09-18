1713a1df6a15bcaddd68c96103f3d1f9d22ce883
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-09-18 21:11:51 +0200

    feat: added deletebykey from storage

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import Logout from "./Logout"
 import { getUser } from '../API/getUser';
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../Context/UserContext";
-import { storageRead, storageSave } from '../Storage/Storage';
+import { storageDelete, storageRead, storageSave } from '../Storage/Storage';
 import { STORAGE_KEY_USER } from '../Storage/storageKeys';
 import { useNavigate } from "react-router-dom";
 import { debug } from 'console';
@@ -16,20 +16,15 @@ import ProfileInfo from './ProfileInfo';
 //Button to clear the translations(delete from browser local storage)
 //Return 
 function  Profile() {
-    
-    //If context.user = null
     const [user, setUser] = useContext(UserContext);
     var uname = "";
     var translations: string[] = [];
+    const navigate = useNavigate();
 
     if(user!==null){ 
         uname = user.username;
         translations = user.translations;
     }
-    //torageSave('translate-user', 'dewaldels');
-
-    const [userLocal] = useState(storageRead(STORAGE_KEY_USER));
-    const navigate = useNavigate();
 
     //debugger
     return (
